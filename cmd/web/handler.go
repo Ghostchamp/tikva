4c9d38a5308ac534a626cec8038b1b825e110bf0
NikL <metrpa777@gmail.com>
2024-08-12 17:02:20 +0300

    first commit

---
+++ cmd/web/handler.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Iternal Sserver Erorr", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func showCataloge(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Отображение определенного товара с ID %d...", id)
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод не дозволен", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Создание нового товара..."))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module tikva.pro/site
+
+go 1.22.5
